13f5c576d5bda249011e0c83a7e1e1f357876f89
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-02 08:28:10 -0300

    Inserção de comentários, finalização de comandos e correções de sintax no arquivo de select

--- select.sql
+++ select.sql
@@ -1,32 +1,33 @@
+
+/*Buscando todos os dados da tabela cliente*/
 select * from cliente;
 
-select * from cliente inner join contato, endereco where cliente.id=contato.id_cliente and cliente.id=endereco.id_cliente;
+/*Buscando dados relacionados filtrados de todos os clientes nas tabelas cliente, contato e endereco */
+select nome, cpf, sexo, data_nascimento, ddd, telefone, email, logradouro, numero, bairro, estado, cidade, complemento, cep from contato inner join cliente inner join endereco on cliente.id=contato.id_cliente and cliente.id=endereco.id_cliente;
 
+/*Buscando dados relacionados filtrados de apensa um cliente nas tabelas cliente, contato e endereco */
+select nome, cpf, sexo, data_nascimento, ddd, telefone, email, logradouro, numero, bairro, estado, cidade, complemento, cep from contato inner join cliente inner join endereco on cliente.id=contato.id_cliente and cliente.id=endereco.id_cliente and cliente.id=6;
 
-select nome, cpf, sexo, data_nascimento, ddd, telefone, email, logradouro, numero, bairro, estado, cidade, complemento, cep from cliente inner join contato, endereco where cliente.id=contato.id_cliente and cliente.id=endereco.id_cliente;
+/*Buscando dados da transportadora*/
+select nome, cnpj, ddd, telefone, email from transportadora inner join contato on contato.id_transportadora=transportadora.id;
 
+/*Buscando dados da transportadora  e o id do carrinho que será realizada a entrega*/
+select nome, cnpj, ddd, telefone, email, id_carrinho from contato inner join transportadora  inner join dados_entrega on contato.id_transportadora=transportadora.id and transportadora.id=dados_entrega.id_transportadora;
 
-select nome, cpf, sexo, data_nascimento, ddd, telefone, email, logradouro, numero, bairro, estado, cidade, complemento, cep from cliente inner join contato, endereco where cliente.id=contato.id_cliente and cliente.id=endereco.id_cliente and cliente.id=6;
+/* Buscando todos os carrinhos com valores unitarios dos produto*/
+select cliente.nome as nome_cliente, cliente.cpf as cliente_cpf, produto.nome as nome_produto, qtd_prod, concat('R$ ' , format(produto.valor, '2')) as valor_unidade_produto  from cliente inner join carrinho inner join produto on carrinho.id_cliente=cliente.id and carrinho.id_produto=produto.id;
 
+/* buscando dados do carrinho de um cliente com valores unitarios dos produtos */
+select cliente.nome as nome_cliente, cpf, produto.nome as nome_produto, qtd_prod as quantidade_produtos, concat('R$ ' , format(produto.valor, '2')) as valor_unidade_produto from cliente inner join carrinho inner join produto on carrinho.id_cliente=cliente.id and carrinho.id_produto=produto.id and cliente.id=1;
 
-select nome, cnpj, ddd, telefone, email, id_carrinho from transportadora inner join contato, dados_entrega where contato.id_transportadora=transportadora.id and transportadora.id=dados_entrega.id_transportadora;
-
-
-/* Buscando todos os carrinhos com valores individuais*/
-select cliente.nome as nome_cliente, cliente.cpf as cliente_cpf, produto.nome as nome_produto, produto.valor as valor_unidade_produto  from carrinho inner join cliente, produto where carrinho.id_cliente=cliente.id and carrinho.id_produto=produto.id;
-
-/* buscando dados do carrinho de um cliente com valores individuais */
-select cliente.nome as nome_cliente, cpf, produto.nome as nome_produto, produto.valor as valor_unidade_produto from carrinho inner join cliente, produto where carrinho.id_cliente=cliente.id and carrinho.id_produto=produto.id and cliente.id=1;
+/* Buscando carrinho de todosos clientes com valor total do carrinho */
+select cliente.nome as nome_cliente, cpf, SUM(qtd_prod) as quantidade_produtos,  concat('R$', format(SUM(valor*qtd_prod), 2))  as valor_total_carrinho from cliente inner join carrinho inner join produto on carrinho.id_produto=produto.id and carrinho.id_cliente = cliente.id group by cliente.id;
 
 /* Buscando carrinho de um cliente com valor total do carrinho */
-select cliente.nome as nome_cliente, cpf, SUM(valor) as valor_total_carrinho from carrinho inner join produto, cliente where carrinho.id_produto=produto.id and carrinho.id_cliente = cliente.id and cliente.id=1 ;
+select cliente.nome as nome_cliente, cpf, SUM(qtd_prod) as quantidade_produtos, concat('R$', format(SUM(valor*qtd_prod), 2)) as valor_total_carrinho from cliente inner join carrinho inner join produto on carrinho.id_produto=produto.id and carrinho.id_cliente = cliente.id and cliente.id=1 ;
 
- 
- 
- select * from historico_compra inner join carrinho, nota_fiscal, pagamento where historico_compra.id_carrinho=carrinho.id and  historico_compra.id_nota_fiscal=nota_fiscal.id and historico_compra.id_pagamento=pagamento.id;
+/*Buscando Produtos em estqoue*/
+select produto.nome as nome_produto, fabricante.nome as fabricante, produto.descricao as descricao, estoque.qtd as quantidade_estoque  from estoque inner join produto inner join fabricante on produto.id = estoque.id_produto and fabricante.id = produto.id_fabricante;
 
-select * from cliente inner join carrinho, produto where cliente.id=carrinho.id_cliente and produto.id=carrinho.id_produto;
-
-
-/*Buscando todas as notas fiscais*/
-select * from nota_fiscal inner join cliente where nota_fiscal.id_cliente=cliente.id;
+/*Buscando histórico de atendiomentos do suporte cliente*/
+select cliente.nome as nome_cliente, descricao, data_atend as data_atendimento, atendente from cliente inner join suporte_cliente on cliente.id = suporte_cliente.id_cliente;
